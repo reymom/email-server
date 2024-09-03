080989771765340cba714a2409b91d9a58d9436f
reymom <ramon_gs95@hotmail.com>
2024-09-03 08:36:18 +0200

    add send recovery email

--- emailServer.js
+++ emailServer.js
@@ -101,6 +101,70 @@ app.post("/send-confirmation-email", (req, res) => {
   });
 });
 
+app.post("/send-password-reset-email", (req, res) => {
+  const { to, resetToken, domain } = req.body;
+
+  const htmlContent = `
+        <!DOCTYPE html>
+        <html lang="en">
+        <head>
+            <meta charset="UTF-8">
+            <meta name="viewport" content="width=device-width, initial-scale=1.0">
+            <title>Email Confirmation</title>
+            <style>
+                body { font-family: Arial, sans-serif; background-color: #f4f4f4; margin: 0; padding: 0; color: #333; }
+                .container { width: 100%; max-width: 600px; margin: 0 auto; background-color: #ffffff; padding: 20px; box-shadow: 0 0 10px rgba(0, 0, 0, 0.1); }
+                .header { text-align: center; padding: 20px; }
+                .header img { width: 150px; }
+                .content { padding: 10px; text-align: center; }
+                .content h1 { font-size: 24px; color: #333333; }
+                .content p { font-size: 16px; color: #777777; }
+                .content .code { font-size: 32px; font-weight: bold; color: #4CAF50; background-color: #f4f4f4; padding: 10px; display: inline-block; border-radius: 5px; margin-top: 10px; }
+                .button-container { margin-top: 20px; }
+                .button { background-color: #4CAF50; color: white; padding: 15px 20px; text-align: center; text-decoration: none; display: inline-block; border-radius: 5px; font-size: 16px; }
+                .footer { text-align: center; margin-top: 30px; padding: 20px; background-color: #f4f4f4; color: #777777; font-size: 14px; }
+            </style>
+        </head>
+        <body>
+            <div class="container">
+                <div class="header">
+                    <img src="https://raw.githubusercontent.com/reymom/ic2P2ramp/develop/assets/p2ploan.png" alt="ic2P2ramp">
+                </div>
+                <div class="content">
+                    <div>
+                        <h1>Reset Your Password</h1>
+                        <p>Please click the button below to reset your password:</p>
+                        <div class="button-container">
+                            <a href="${domain}/reset-password?token=${resetToken}" class="button">Reset Password</a>
+                        </div>
+                    </div>
+                    <div class="footer">
+                        <p>If you have any questions, feel free to contact us at</p>
+                        <p>Barcelona, Spain</p>
+                    </div>
+                </div>
+            </div>
+        </body>
+        </html>
+    `;
+
+  const mailOptions = {
+    from: process.env.EMAIL_USER,
+    to: to,
+    subject: "Password Reset",
+    html: htmlContent,
+  };
+
+  transporter.sendMail(mailOptions, (error, info) => {
+    if (error) {
+      console.error("Error sending email:", error);
+      return res.status(500).send(error.toString());
+    }
+    console.log("Email sent:", info.response);
+    res.status(200).send("Password reset email sent: " + info.response);
+  });
+});
+
 // Start the server
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
