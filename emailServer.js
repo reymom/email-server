699723e27c271b626142e1331647efef9427c23a
reymom <ramon_gs95@hotmail.com>
2024-09-10 10:52:54 +0200

    refractor email template

--- emailServer.js
+++ emailServer.js
@@ -37,52 +37,56 @@ app.use((req, res, next) => {
   next();
 });
 
+// Common Email Template Function
+const generateEmailTemplate = (content) => `
+  <!DOCTYPE html>
+  <html lang="en">
+  <head>
+      <meta charset="UTF-8">
+      <meta name="viewport" content="width=device-width, initial-scale=1.0">
+      <title>icRamp Email</title>
+      <style>
+          body { font-family: Arial, sans-serif; background-color: #f4f4f4; margin: 0; padding: 0; color: #333; }
+          .container { width: 100%; max-width: 600px; margin: 0 auto; background-color: #ffffff; padding: 20px; box-shadow: 0 0 10px rgba(0, 0, 0, 0.1); }
+          .header { text-align: center; padding: 6px; }
+          .header img { width: 250px; }
+          .content { padding: 10px; text-align: center; }
+          .content h1 { font-size: 24px; color: #333333; }
+          .content p { font-size: 16px; color: #777777; }
+          .button-container { margin-top: 20px; }
+          .button { background-color: #4CAF50; color: white; padding: 15px 20px; text-align: center; text-decoration: none; display: inline-block; border-radius: 5px; font-size: 16px; }
+          .footer { text-align: center; margin-top: 30px; padding: 20px; background-color: #f4f4f4; color: #777777; font-size: 14px; }
+      </style>
+  </head>
+  <body>
+      <div class="container">
+          <div class="header">
+              <img src="https://raw.githubusercontent.com/reymom/ic2P2ramp/ic-prod-network/src/assets/icR-logo-rectangular.png" alt="icRamp">
+          </div>
+          <div class="content">
+              ${content}
+          </div>
+          <div class="footer">
+              <p>If you have any questions, feel free to contact us at</p>
+              <p>Barcelona, Spain</p>
+          </div>
+      </div>
+  </body>
+  </html>
+`;
+
 // Route to send confirmation email
 app.post("/send-confirmation-email", (req, res) => {
   const { to, token, domain } = req.body;
 
-  const htmlContent = `
-        <!DOCTYPE html>
-        <html lang="en">
-        <head>
-            <meta charset="UTF-8">
-            <meta name="viewport" content="width=device-width, initial-scale=1.0">
-            <title>Email Confirmation</title>
-            <style>
-                body { font-family: Arial, sans-serif; background-color: #f4f4f4; margin: 0; padding: 0; color: #333; }
-                .container { width: 100%; max-width: 600px; margin: 0 auto; background-color: #ffffff; padding: 20px; box-shadow: 0 0 10px rgba(0, 0, 0, 0.1); }
-                .header { text-align: center; padding: 20px; }
-                .header img { width: 150px; }
-                .content { padding: 10px; text-align: center; }
-                .content h1 { font-size: 24px; color: #333333; }
-                .content p { font-size: 16px; color: #777777; }
-                .content .code { font-size: 32px; font-weight: bold; color: #4CAF50; background-color: #f4f4f4; padding: 10px; display: inline-block; border-radius: 5px; margin-top: 10px; }
-                .button-container { margin-top: 20px; }
-                .button { background-color: #4CAF50; color: white; padding: 15px 20px; text-align: center; text-decoration: none; display: inline-block; border-radius: 5px; font-size: 16px; }
-                .footer { text-align: center; margin-top: 30px; padding: 20px; background-color: #f4f4f4; color: #777777; font-size: 14px; }
-            </style>
-        </head>
-        <body>
-            <div class="container">
-                <div class="header">
-                    <img src="https://raw.githubusercontent.com/reymom/ic2P2ramp/ic-prod-network/src/assets/icRamp-logo.png" alt="icRamp">
-                </div>
-                <div class="content">
-                    <h1>You Are Almost There!</h1>
-                    <p>Only one step left to become a part of icRamp. Please enter this verification code in the window where you started creating your account or click the button below.</p>
-                    <div class="code">${token}</div>
-                    <div class="button-container">
-                        <a href="${domain}/confirm-email?token=${token}" class="button">Confirm my email</a>
-                    </div>
-                </div>
-                <div class="footer">
-                    <p>If you have any questions, feel free to contact us at</p>
-                    <p>Barcelona, Spain</p>
-                </div>
-            </div>
-        </body>
-        </html>
-    `;
+  const htmlContent = generateEmailTemplate(`
+    <h1>You Are Almost There!</h1>
+    <p>Only one step left to become a part of icRamp. Please enter this verification code in the window where you started creating your account or click the button below.</p>
+    <div class="code">${token}</div>
+    <div class="button-container">
+        <a href="${domain}/confirm-email?token=${token}" class="button">Confirm my email</a>
+    </div>
+  `);
 
   const mailOptions = {
     from: `"icRamp Team" ${process.env.EMAIL_USER}`,
@@ -104,49 +108,13 @@ app.post("/send-confirmation-email", (req, res) => {
 app.post("/send-password-reset-email", (req, res) => {
   const { to, resetToken, domain } = req.body;
 
-  const htmlContent = `
-        <!DOCTYPE html>
-        <html lang="en">
-        <head>
-            <meta charset="UTF-8">
-            <meta name="viewport" content="width=device-width, initial-scale=1.0">
-            <title>Email Confirmation</title>
-            <style>
-                body { font-family: Arial, sans-serif; background-color: #f4f4f4; margin: 0; padding: 0; color: #333; }
-                .container { width: 100%; max-width: 600px; margin: 0 auto; background-color: #ffffff; padding: 20px; box-shadow: 0 0 10px rgba(0, 0, 0, 0.1); }
-                .header { text-align: center; padding: 20px; }
-                .header img { width: 150px; }
-                .content { padding: 10px; text-align: center; }
-                .content h1 { font-size: 24px; color: #333333; }
-                .content p { font-size: 16px; color: #777777; }
-                .content .code { font-size: 32px; font-weight: bold; color: #4CAF50; background-color: #f4f4f4; padding: 10px; display: inline-block; border-radius: 5px; margin-top: 10px; }
-                .button-container { margin-top: 20px; }
-                .button { background-color: #4CAF50; color: white; padding: 15px 20px; text-align: center; text-decoration: none; display: inline-block; border-radius: 5px; font-size: 16px; }
-                .footer { text-align: center; margin-top: 30px; padding: 20px; background-color: #f4f4f4; color: #777777; font-size: 14px; }
-            </style>
-        </head>
-        <body>
-            <div class="container">
-                <div class="header">
-                    <img src="https://raw.githubusercontent.com/reymom/ic2P2ramp/ic-prod-network/src/assets/icRamp-logo.png" alt="icRamp">
-                </div>
-                <div class="content">
-                    <div>
-                        <h1>Reset Your Password</h1>
-                        <p>Please click the button below to reset your password:</p>
-                        <div class="button-container">
-                            <a href="${domain}/reset-password?token=${resetToken}" class="button">Reset Password</a>
-                        </div>
-                    </div>
-                    <div class="footer">
-                        <p>If you have any questions, feel free to contact us at</p>
-                        <p>Barcelona, Spain</p>
-                    </div>
-                </div>
-            </div>
-        </body>
-        </html>
-    `;
+  const htmlContent = generateEmailTemplate(`
+    <h1>Reset Your Password</h1>
+    <p>Please click the button below to reset your password:</p>
+    <div class="button-container">
+        <a href="${domain}/reset-password?token=${resetToken}" class="button">Reset Password</a>
+    </div>
+  `);
 
   const mailOptions = {
     from: `"icRamp Team" ${process.env.EMAIL_USER}`,
